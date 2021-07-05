d764840c4a56656334cde847d957e72eff28a37d
Mansurjon Muhammadovich Kamolov <64670025+Mansurjon1112@users.noreply.github.com>
2021-07-05 10:09:13 +0500

    Update For03.py

--- For03.py
+++ For03.py
@@ -1,5 +1,11 @@
+""" 
+For3. a va b butun sonlari berilgan (a < b). a va b sonlari orasidagi barcha butun sonlarni (a va b
+dan tashqari) kamayish tartibida chiqaruvchi va chiqarilgan sonlar sonini chiqaruvchi progma
+tuzilsin.
+"""
+
 a=int(input('a='))
 b=int(input('b='))
-for i in range (b,a-1,-1):
+for i in range (b-1,a,-1):
     print(i)
-print('Oradagi sonlar soni: ',b-a+1)
+print('Oradagi sonlar soni: ',b-a-1)
